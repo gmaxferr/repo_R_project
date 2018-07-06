658e9ff317b894ab4d5a80058b15ecd5f65c11db
1150623 <1150623@isep.ipp.pt>
2018-07-06 01:26:19 +0100

    aliea a) e b) parciais

--- fiabilidade.R
+++ fiabilidade.R
@@ -63,9 +63,9 @@ toDate <- function(date){
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-# ===============================
-# ==== MAIN USABILITY SCRIPT ====
-# ===============================
+# ========================================================================================================================
+# ====                                  MAIN USABILITY SCRIPT                                                         ====
+# ========================================================================================================================
 
 # Esta package permite processamento paralelo de operacoes,
 # o que facilita e acelera calculos necessarios
@@ -101,10 +101,131 @@ if(!file.exists("./Resources/vpn_sessions_2.csv")){
   write.csv(vpn_sessions, "./Resources/vpn_sessions_2.csv")
 }else {
   vpn_sessions = read.csv("./Resources/vpn_sessions_2.csv", sep=',')
+  vpn_sessions = vpn_sessions[2:6]
 }
 
 # View(vpn_sessions)
 
+
+
+# ------------------------------------
+# Exercício 2
+# ------------------------------------
+
+
+# Alínea a)
+# - - - - - - 
+
+# TOTAL - SEMPRE
+
+all_vsrv8 = subset(vpn_sessions, Servidor=="vsrv8")
+all_vsrv10 = subset(vpn_sessions, Servidor=="vsrv10")
+all_vsrv16 = subset(vpn_sessions, Servidor=="vsrv16")
+all_vsrv17 = subset(vpn_sessions, Servidor=="vsrv17")
+
+# VSVR10 - 2017
+
+startDate = joinDateTime("2017-01-01", "00:00:00")
+endDate = joinDateTime("2017-12-31","23:59:00")
+index = c()
+cont = 1
+while(cont <= nrow(all_vsrv10)){
+  ini = toDate(all_vsrv10[[cont,4]])
+  if(subtractDate(startDate, ini) <= 0 && subtractDate(endDate, ini) >= 0){
+    index = c(index, cont)
+  }
+  cont = cont +1
+}
+vsrv10 = all_vsrv10[index,]
+
+# VSVR16 - Marco 2017
+
+startDate = joinDateTime("2017-03-01", "00:00:00")
+endDate = joinDateTime("2017-03-31","23:59:00")
+index = c()
+cont = 1
+while(cont <= nrow(all_vsrv16)){
+  ini = toDate(all_vsrv16[[cont,4]])
+  if(subtractDate(startDate, ini) <= 0 && subtractDate(endDate, ini) >= 0){
+    index = c(index, cont)
+  }
+  cont = cont +1
+}
+vsrv16 = all_vsrv16[index,]
+
+# VSVR17 - 28 Fevereiro 2017
+
+startDate = joinDateTime("2017-02-28", "00:00:00")
+endDate = joinDateTime("2017-02-28","23:59:00")
+index = c()
+cont = 1
+while(cont <= nrow(all_vsrv17)){
+  ini = toDate(all_vsrv17[[cont,4]])
+  if(subtractDate(startDate, ini) <= 0 && subtractDate(endDate, ini) >= 0){
+    index = c(index, cont)
+  }
+  cont = cont +1
+}
+vsrv17 = all_vsrv17[index,]
+
+
+
+
+
+
+
+# Alínea b)
+# - - - - - - 
+
+
+# Inicio de Janeiro e Fim de Fevereira: utilizadas para limitar os valores
+startDate = joinDateTime("2017-01-01", "00:00:00")
+endDate = joinDateTime("2017-02-28","23:59:00")
+index = c()
+cont = 1
+while(cont <= nrow(all_vsrv8)){
+ 
+  ini = toDate(all_vsrv8[[cont,4]])
+  if(subtractDate(startDate, ini) <= 0 && subtractDate(endDate, ini) >= 0){
+    index = c(index, cont)
+  }
+  
+  cont = cont +1
+}
+
+# Fazemos depois a filtragem consoante os indices encontrados que tem o valores
+# que pretendemos.
+vsrv8 = all_vsrv8[index,]
+View(vsrv8)
+print(vsrv8[vsrv8[5] == 0,])
+# Calculo do tempo de operacao e numero de falhas
+
+n_falhas = nrow(vsrv8[vsrv8[5] == 0,]) + nrow(vsrv8[vsrv8[5] == 1,])
+tempo_total = subtractDate(tail(vsrv8, 1)$Data_Ini, head(vsrv8, 1)$Data_Ini)
+
+func = n_falhas / tempo_total
+
+# Alínea c)
+# - - - - - - 
+
+
+
+
+# Alínea d)
+# - - - - - - 
+
+
+
+
+
+# Alínea e)
+# - - - - - - 
+
+
+
+
+# Alínea f)
+# - - - - - - 
 
 
 
